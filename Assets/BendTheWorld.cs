de22e7192ef3012038b3a6db6c7701be0f828672
zsacul <zsacul@gmail.com>
2020-11-28 17:36:48 +0100

    lepiej

--- Assets/BendTheWorld.cs
+++ Assets/BendTheWorld.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class BendTheWorld : MonoBehaviour
 {
-    const int n = 24;
-    List<BendItem> objs = new List<BendItem>();
+    const int n = 32;
+    //List<BendItem> objs = new List<BendItem>();
     List<BendItem> twisted = new List<BendItem>();
     public float fOffset = 180.0f;
      
@@ -22,6 +22,7 @@ public class BendTheWorld : MonoBehaviour
         int nCenterID = (int)xPlayer;
 
         twisted.Clear();
+        //objs.Clear();
 
         var orginals = GameObject.Find("Orginal");
         int nOrginalsChilds = orginals.transform.childCount;
@@ -29,18 +30,23 @@ public class BendTheWorld : MonoBehaviour
         int startID = nCenterID - n/2;
         int endID   = nCenterID + n/2;
         
-        if (startID<0) startID=0;
-        if (endID>nOrginalsChilds-1) endID=nOrginalsChilds-1;
+        //if (startID<0) startID=0;
+        //if (endID>nOrginalsChilds-1) endID=nOrginalsChilds-1;
         
-        for (int i=startID;i<endID;i++)
+        for (int id=startID;id<endID;id++)
         {
-            string name = "ground00 (" + i.ToString() + ")";
+            string num = id.ToString();
+            if (num.Length<2) num = "0" + num;
+            string name = "ground00 (" + num + ")";
             Transform tr = orginals.transform.Find(name);
+            if (tr==null) continue;
             
+            /*
             BendItem bendObj;
             bendObj.transform = tr;
-            bendObj.nID = i;
+            bendObj.nID = id;
             objs.Add(bendObj);
+            */
 
             Vector3 posG = tr.position;
             posG.z = 5.0f;
@@ -50,16 +56,41 @@ public class BendTheWorld : MonoBehaviour
 
             BendItem bendItem;
             bendItem.transform = newTransf;
-            bendItem.nID = i;           
+            bendItem.nID = id;           
             twisted.Add(bendItem);
         }
-        
+  /*      
         BendItem bendPla;
         bendPla.transform = orginals.transform.Find("Player").transform;
         bendPla.nID = nCenterID;
         objs.Add(bendPla);
-        
+*/
+        GameObject others = GameObject.Find("OtherObjects");
 
+        for (int i=0;i<others.transform.childCount;i++)
+        {
+            Transform child = others.transform.GetChild(i);
+            int id = (int)(child.position.x+0.5f);
+
+            if (id<startID || id>=endID) continue;
+
+            Vector3 posG = child.position;
+            posG.z = 5.0f;
+
+            Transform newTransf = Instantiate(child,posG,child.rotation,GameObject.Find("Twisted").transform);
+
+            if (newTransf.GetComponent<Rigidbody>()!=null)
+            Destroy(newTransf.GetComponent<Rigidbody>());
+            //newTransf.GetComponent<Rigidbody>().isKinematic = true;
+
+            BendItem bendItem;
+            bendItem.transform = newTransf;
+            bendItem.nID = id;
+            twisted.Add(bendItem);
+
+        }
+        
+/*
         Transform t = orginals.transform.Find("Player");
         
         Vector3 pos = t.position;
@@ -79,7 +110,7 @@ public class BendTheWorld : MonoBehaviour
         bendPlayer.nID = nCenterID;
 
         twisted.Add(bendPlayer);
-
+*/
     }
 
     public void Start()
@@ -88,6 +119,7 @@ public class BendTheWorld : MonoBehaviour
 
      Vector3 GetCircle(Vector3 center,float radius,float ang)
      {
+        while (ang>360.0f) ang-=360.0f;
         Vector3 pos = new Vector3();
         pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
         pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
@@ -112,16 +144,23 @@ public class BendTheWorld : MonoBehaviour
 
         CloneAll();
 
+        //string ss = "";
+
         if (twisted.Count>0)
         {
-            for (int i=0;i<twisted.Count-1;i++)
+            for (int i=0;i<twisted.Count;i++)
             {
-                Transform g = objs[i].transform;
-                Vector3 center = new Vector3(0,0,-5.0f);
-                float xPlayerDel = xPlayer - (int)xPlayer;
-                float fAngle = fOffset + ((objs[i].nID - xPlayer)*360.0f)/(float)n;
+                int id = twisted[i].nID;
+                Transform g = twisted[i].transform;
+//              int id = objs[i].nID;
+//              Transform g = objs[i].transform;
 
-                Vector3 circ = GetCircle(center,n/(2.0f*Mathf.PI),-fAngle);
+                Vector3 center = new Vector3(0,0,-g.position.z);
+                float xPlayerDel = xPlayer - (int)xPlayer;
+                float fAngleSpan = 360.0f/(float)n;
+                float fAngle = fOffset + (id-xPlayer)*fAngleSpan;
+
+                Vector3 circ = GetCircle(center,n/(2.0f*Mathf.PI)-g.position.y,-fAngle);
                 Vector3 pos = circ;
                 Quaternion rot = Quaternion.LookRotation(new Vector3(0,0,1.0f),center-circ);               
                                 
@@ -130,5 +169,6 @@ public class BendTheWorld : MonoBehaviour
 
             //Twist((BendItem) twisted[twisted.Count-1],xPlayer);
         }
+        //Debug.Log(ss);
     }
 }
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            Vector3 pos = transform.position;
+            pos.x+=5.0f*Time.deltaTime;
+            transform.SetPositionAndRotation(pos,transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            Vector3 pos = transform.position;
+            pos.x-=5.0f*Time.deltaTime;
+            transform.SetPositionAndRotation(pos,transform.rotation);
+        }
+
+    }
+}
