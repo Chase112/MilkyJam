6384dfbbbaa969e5e517d11823ce782c28e3e1f7
zsacul <zsacul@gmail.com>
2020-11-29 07:09:11 +0100

    Rotation

--- Assets/MountainRotation.cs
+++ Assets/MountainRotation.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class MountainRotation : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fAngle = GameObject.Find("Player").transform.position.x;
-        var rot = Quaternion.Euler(-90.0f,fAngle,0.0f);
-        transform.SetPositionAndRotation(transform.position,rot);
+        if (player.active)
+        {
+            float fAngle = player.transform.position.x;        
+            var rot = Quaternion.Euler(-90.0f,fAngle,0.0f);
+            transform.SetPositionAndRotation(transform.position,rot);
+        }
     }
 }
