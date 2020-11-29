e6744374039b3cf894ef4b0b2b4239dbba52bdce
Risist <risistt@gmail.com>
2020-11-29 07:52:49 +0100

    dcs

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,15 +6,29 @@ public class GameController : MonoBehaviour
 {
     public GameObject[] turnOn_onDetected;
     public GameObject[] turnOff_onDetected;
+    public GameObject[] turnOn_Delayed_onDetected;
+
+    Coroutine coroutine;
 
     public void LightHit()
     {
-        Debug.Log("PlayerHit");
-
         foreach (var it in turnOn_onDetected)
             it.SetActive(true);
 
         foreach (var it in turnOff_onDetected)
             it.SetActive(false);
+
+        if(coroutine == null)
+            coroutine = StartCoroutine(Delay(4.0f));
+    }
+
+    IEnumerator Delay(float time)
+    {
+        yield return new WaitForEndOfFrame();
+        
+        foreach (var it in turnOn_Delayed_onDetected)
+            it.SetActive(true);
+
+        coroutine = null;
     }
 }
